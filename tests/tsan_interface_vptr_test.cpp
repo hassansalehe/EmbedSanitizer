ad64d4774dadd12f3cac025f674091b5f10b9a77
Hassan @Ubuntu <hassansalehe@gmail.com>
2021-07-18 22:16:18 +0200

    Added empty file for tsan virtual pointer-related

---
+++ tests/tsan_interface_vptr_test.cpp
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////
+//
+// Copyright (c) 2017 - 2021  Hassan Salehe Matar
+//
+// See LICENSE file for information about the license.
+//
+//
+// Unit tests for tsan_interface virtual pointer related functions.
+//
+////////////////////////////////////////////////////
+
+#include <gtest/gtest.h>
+
+#include <functional>
+#include <memory>
+#include <vector>
+#include <unordered_map>
+#include <thread>
+
+#include "etsan/tsan_interface.h"
+
+TEST(TsanInterfaceTestFixture, CheckTsanRreadWriteWithConcurrencyAndRace) {
+}
+
